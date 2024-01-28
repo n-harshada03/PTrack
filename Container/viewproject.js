6d23c5d68dee8e1e0c628393f64bdfe80a907034
karan-walekar <santosh.karan21@vit.edu>
2024-01-28 15:34:37 +0530

    View Projects Complete Guys. Party!!!!

---
+++ Container/viewproject.js
@@ -0,0 +1,11 @@
+// JavaScript code for viewprojects.js
+// Add any necessary logic or functionality for the viewprojects page here
+// For example, handling the "Edit Project" button click event
+
+const editProjectButton = document.getElementById('edit-project-button');
+
+editProjectButton.addEventListener('click', () => {
+    // Your logic to handle the "Edit Project" button click goes here
+    // You can open the project edit form or perform other actions
+    console.log('Edit Project button clicked');
+});
